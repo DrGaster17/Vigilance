c66f67440e8b23c05ff47f9f5dd245dec07b8e4b
Doctor Gaster <77633241+DrGaster17@users.noreply.github.com>
2021-01-23 17:14:35 +0100

    v5.5.4

--- Vigilance/Patches/Events/PlayerStats_Roundrestart.cs
+++ Vigilance/Patches/Events/PlayerStats_Roundrestart.cs
@@ -1,144 +1,68 @@
-﻿using System;
-using GameCore;
-using Harmony;
-using Vigilance.API;
+﻿using Harmony;
 using Dissonance.Integrations.MirrorIgnorance;
-using Mirror;
-using MEC;
-using UnityEngine;
+using System;
+using Vigilance.API;
 
 namespace Vigilance.Patches.Events
 {
     [HarmonyPatch(typeof(PlayerStats), nameof(PlayerStats.Roundrestart))]
     public static class PlayerStats_Roundrestart
     {
-        public static bool Prefix(PlayerStats __instance) => Restart.Commit(__instance, ConfigManager.FastRestart ? Restart.RestartType.Fast : Restart.RestartType.Normal);
-    }
-
-    public static class Restart
-    {
-        public static bool RestartMessageShown = false;
-
-        public enum RestartType
+        public static bool Prefix(PlayerStats __instance)
         {
-            Normal,
-            Fast
-        }
-
-        public static void ServerRestart()
-        {
-            Log.Add("RESTART", $"The server is about to restart!", ConsoleColor.Red);
-            Environment.Cache.LocalStats.Roundrestart();
-            Timing.CallDelayed(1.5f, () => Application.Quit());
-        }
-
-        public static bool Commit(PlayerStats ps, RestartType type)
-        {
-            try
-            {
-                if (!RestartMessageShown)
+			try
+			{
+				Log.Add("The round is restarting!");
+				CustomLiteNetLib4MirrorTransport.DelayConnections = true;
+				CustomLiteNetLib4MirrorTransport.UserIdFastReload.Clear();
+				RagdollManager_SpawnRagdoll.Owners.Clear();
+				RagdollManager_SpawnRagdoll.Ragdolls.Clear();
+				Inventory_CallCmdDropItem.Pickups.Clear();
+				foreach (Player player in Server.Players)
                 {
-                    Log.Add("RESTART", $"The round is restarting! Using a {type} restart.", ConsoleColor.Magenta);
-                    RestartMessageShown = true;
+					Environment.OnPlayerLeave(player, out bool d);
+                }
+				Server.PlayerList.Reset();
+				IdleMode.PauseIdleMode = true;
+
+				foreach (MirrorIgnorancePlayer mip in UnityEngine.Object.FindObjectsOfType<MirrorIgnorancePlayer>())
+                {
+					mip.OnDisable();
                 }
 
-                Round.CurrentState = Enums.RoundState.Restarting;
-                Environment.OnRoundRestart();
-                PauseAndDelay();
+				if (CustomNetworkManager.EnableFastRestart && ConfigManager.FastRestart)
+				{
+					foreach (ReferenceHub referenceHub in ReferenceHub.GetAllHubs().Values)
+					{
+						if (!referenceHub.isDedicatedServer)
+						{
+							try
+							{
+								CustomLiteNetLib4MirrorTransport.UserIdFastReload.Add(referenceHub.characterClassManager.UserId);
+							}
+							catch (Exception ex)
+							{
+								ServerConsole.AddLog("Exception occured during processing online player list for Fast Restart: " + ex.Message, ConsoleColor.Yellow);
+							}
+						}
+					}
 
-                if (type == RestartType.Fast)
-                {
-                    DoFastRestart(ps);
-                }
-                else
-                {
-                    DoNormalRestart(ps);
-                }
-
-                Clear();
-                return false;
-            }
-            catch (Exception e)
+					__instance.RpcFastRestart();
+					PlayerStats.StaticChangeLevel(false);
+				}
+				else
+				{
+					if (ServerStatic.StopNextRound == ServerStatic.NextRoundAction.DoNothing)
+						__instance.RpcRoundrestart(PlayerPrefsSl.Get("LastRoundrestartTime", 5000) / 1000f, true);
+					__instance.Invoke("ChangeLevel", 2.5f);
+				}
+				return false;
+			}
+			catch (Exception e)
             {
-                Log.Add("RESTART", "An error occured while restarting the round!", LogType.Error);
-                Log.Add("RESTART", e);
-                return true;
+				Log.Add(e);
+				return true;
             }
-        }
-
-        public static void PauseAndDelay()
-        {
-            CustomLiteNetLib4MirrorTransport.DelayConnections = true;
-            CustomLiteNetLib4MirrorTransport.UserIdFastReload.Clear();
-            foreach (MirrorIgnorancePlayer pl in Map.FindObjects<MirrorIgnorancePlayer>())
-                pl?.OnDisable();
-        }
-
-        public static void DoNormalRestart(PlayerStats ps)
-        {
-            SendRestartRpc(ps.ccm._hub);
-            ChangeScene(ps);
-        }
-
-        public static bool DoFastRestart(PlayerStats ps)
-        {
-            try
-            {
-                foreach (ReferenceHub referenceHub in ReferenceHub.GetAllHubs().Values)
-                {
-                    if (!referenceHub.isDedicatedServer)
-                    {
-                        try
-                        {
-                            CustomLiteNetLib4MirrorTransport.UserIdFastReload.Add(referenceHub.characterClassManager.UserId);
-                        }
-                        catch (Exception e)
-                        {
-                            Log.Add("RESTART", "An exception occured while trying to do a fast restart.", LogType.Error);
-                            Log.Add("RESTART", e);
-                        }
-                    }
-                }
-
-                ps?.RpcFastRestart();
-                ChangeScene(ps);
-                return false;
-            }
-            catch (Exception e)
-            {
-                Log.Add("RESTART", "An exception occured while trying to do a fast restart.", LogType.Error);
-                Log.Add("RESTART", e);
-                return true;
-            }
-        }
-
-        public static void ChangeScene(PlayerStats ps)
-        {
-            ps.Invoke("ChangeLevel", 2.5f);
-        }
-
-        public static void Clear()
-        {
-            RagdollManager_SpawnRagdoll.Owners.Clear();
-            RagdollManager_SpawnRagdoll.Ragdolls.Clear();
-            Inventory_CallCmdDropItem.Pickups.Clear();
-
-            if (ConfigManager.DisableLocksOnRestart)
-            {
-                RoundSummary.RoundLock = false;
-                RoundStart.LobbyLock = false;
-            }
-
-            Server.PlayerList.Reset();
-        }
-
-        public static void SendRestartRpc(ReferenceHub hub)
-        {
-            NetworkWriter writer = NetworkWriterPool.GetWriter();
-            writer.WriteSingle(PlayerPrefsSl.Get("LastRoundrestartTime", 5000) / 1000f);
-            writer.WriteBoolean(true);
-            hub.playerStats.SendRPCInternal(typeof(PlayerStats), "RpcRoundrestart", writer, 0);
-            NetworkWriterPool.Recycle(writer);
-        }
+		}
     }
 }
