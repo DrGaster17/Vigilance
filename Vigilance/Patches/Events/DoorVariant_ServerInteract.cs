fa31be5cd80938727db32ee301c088aa80eede50
Doctor Gaster <77633241+DrGaster17@users.noreply.github.com>
2021-02-09 13:04:07 +0100

    Patches

--- Vigilance/Patches/Events/DoorVariant_ServerInteract.cs
+++ Vigilance/Patches/Events/DoorVariant_ServerInteract.cs
@@ -2,6 +2,7 @@
 using Harmony;
 using Vigilance.API;
 using Vigilance.Extensions;
+using Vigilance.Utilities;
 using Interactables.Interobjects.DoorUtils;
 
 namespace Vigilance.Patches.Events
@@ -15,45 +16,45 @@ namespace Vigilance.Patches.Events
 			{
 				API.Door door = __instance.GetDoor();
 				Player player = Server.PlayerList.GetPlayer(ply);
-				if (door == null || player == null)
-					return true;
-				if (__instance.ActiveLocks > 0)
-				{
-					DoorLockMode mode = DoorLockUtils.GetMode((DoorLockReason)__instance.ActiveLocks);
-					if ((!mode.HasFlagFast(DoorLockMode.CanClose) || !mode.HasFlagFast(DoorLockMode.CanOpen)) && (!mode.HasFlagFast(DoorLockMode.ScpOverride) || ply.characterClassManager.CurRole.team != Team.SCP) && (mode == DoorLockMode.FullLock || (__instance.TargetState && !mode.HasFlagFast(DoorLockMode.CanClose)) || (!__instance.TargetState && !mode.HasFlagFast(DoorLockMode.CanOpen))))
-					{
-						__instance.LockBypassDenied(ply, colliderId);
-						return false;
-					}
-				}
+				if (door == null || player == null) return true;
+				Handling.OnDoorInteract(true, door, player, out bool allow);
 
-				if (ConfigManager.RemoteCard)
-                {
-					if (door.IsAllowed(player, true, colliderId) || ply.characterClassManager.CurClass == RoleType.Scp079)
-                    {
-						Vigilance.Utilities.Handling.OnDoorInteract(true, door, player, out bool allow);
-						if (!allow)
-							return false;
-						door.ChangeState();
-						__instance._triggerPlayer = ply;
-						return false;
-                    }
-                }
-
-				if (__instance.AllowInteracting(ply, colliderId))
+				if (allow)
 				{
-					if (ply.characterClassManager.CurClass == RoleType.Scp079 || door.IsAllowed(player, false, colliderId))
+					if (__instance.ActiveLocks > 0)
 					{
-						Vigilance.Utilities.Handling.OnDoorInteract(true, door, player, out bool allow);
-						if (!allow)
+						DoorLockMode mode = DoorLockUtils.GetMode((DoorLockReason)__instance.ActiveLocks);
+						if ((!mode.HasFlagFast(DoorLockMode.CanClose) || !mode.HasFlagFast(DoorLockMode.CanOpen)) && (!mode.HasFlagFast(DoorLockMode.ScpOverride) || ply.characterClassManager.CurRole.team != Team.SCP) && (mode == DoorLockMode.FullLock || (__instance.TargetState && !mode.HasFlagFast(DoorLockMode.CanClose)) || (!__instance.TargetState && !mode.HasFlagFast(DoorLockMode.CanOpen))))
+						{
+							__instance.LockBypassDenied(ply, colliderId);
 							return false;
-						__instance.NetworkTargetState = !__instance.TargetState;
-						__instance._triggerPlayer = ply;
-						return false;
+						}
 					}
 
-					__instance.PermissionsDenied(ply, colliderId);
-					DoorEvents.TriggerAction(__instance, DoorAction.AccessDenied, ply);
+					if (ConfigManager.RemoteCard)
+					{
+						if (door.IsAllowed(player, true, colliderId) || ply.characterClassManager.CurClass == RoleType.Scp079)
+						{
+							if (!allow) return false;
+							door.ChangeState();
+							__instance._triggerPlayer = ply;
+							return false;
+						}
+					}
+
+					if (__instance.AllowInteracting(ply, colliderId))
+					{
+						if (ply.characterClassManager.CurClass == RoleType.Scp079 || door.IsAllowed(player, false, colliderId))
+						{
+							if (!allow) return false;
+							__instance.NetworkTargetState = !__instance.TargetState;
+							__instance._triggerPlayer = ply;
+							return false;
+						}
+
+						__instance.PermissionsDenied(ply, colliderId);
+						DoorEvents.TriggerAction(__instance, DoorAction.AccessDenied, ply);
+					}
 				}
 
 				return false;
